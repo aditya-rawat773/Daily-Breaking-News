ced2b1db798884128eb82cf400f3909b50033334
aditya-rawat773 <adityarawat.cf@gmail.com>
2021-07-15 19:28:47 +0530

    Navigation done!!

--- app/src/main/java/com/example/dailybreakingnews/MainActivity.kt
+++ app/src/main/java/com/example/dailybreakingnews/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.dailybreakingnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
-    fun todo() {}
+
 }
